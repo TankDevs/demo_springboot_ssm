e5dda0a3c62d40b1fe55851c1b8cc17068391e0c
tank <tankchow@foxmail.com>
2019-01-10 19:53:25 +0800

    小小修改。

---
+++ src/main/java/com/zlh/demo_springboot_ssm/controller/AccoutController.java
@@ -0,0 +1,29 @@
+package com.zlh.demo_springboot_ssm.controller;
+
+import com.zlh.demo_springboot_ssm.domain.Account;
+import com.zlh.demo_springboot_ssm.domain.PersonInfo;
+import com.zlh.demo_springboot_ssm.mapper.AccountMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class AccoutController {
+
+    @Autowired
+    private AccountMapper accountMapper;
+
+    @RequestMapping("/accounts")
+    public ModelAndView getAccounts() {
+        ModelMap map=new ModelMap();
+        List<Account> accounts = accountMapper.selectAll();
+        map.put("accountList",accounts);
+        return new ModelAndView("accounts",map);
+    }
+
+}
--- src/main/java/com/zlh/demo_springboot_ssm/mapper/AccountMapper.java
+++ src/main/java/com/zlh/demo_springboot_ssm/mapper/AccountMapper.java
@@ -3,6 +3,8 @@ package com.zlh.demo_springboot_ssm.mapper;
 import com.zlh.demo_springboot_ssm.domain.Account;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 //在此添加注解@Mapper或者直接启动类上面添加注解@MapperScan("com.zlh.demo_springboot_ssm.mapper")
 // 建议使用后者，不然每个mapper加个注解也挺麻烦的
 @Mapper
@@ -15,7 +17,11 @@ public interface AccountMapper {
 
     Account selectByPrimaryKey(String accounId);
 
+    List<Account> selectAll();
+
     int updateByPrimaryKeySelective(Account record);
 
+
     int updateByPrimaryKey(Account record);
+
 }
